8a0a874d9e9d00f4b5ff02155de343e68b821b29
Satit Porntepanon <st_satitza@hotmail.com>
2022-01-08 10:11:09 +0700

    add home controller

---
+++ DotnetSecurity/DotnetSecurity/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasicAuthentication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        { 
+            return Ok("Index Page");
+        }
+
+        [Authorize]
+        public IActionResult Secret()
+        {
+           return Ok("Secret");
+        }
+    }
+}
